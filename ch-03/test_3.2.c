f8cf692544516941886c0a7ceea41212e30390c3
root <root@XT047-hs.(none)>
2017-02-23 19:00:17 +0800

    2017-02-23

---
+++ ch-03/test_3.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	char num;
+	printf("Please type a letter:");
+	scanf("%c",&num);
+	printf("the letter is %d\n",num);
+	return 0;
+}
---
+++ ch-03/test_3.4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	float num; 
+	scanf("%f",&num);
+	printf("The input is %f or %e.\n",num,num);
+	return 0;
+}
