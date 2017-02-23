6c87afeb0ced5f8c9f69ebba3441f5c5c79c2b45
root <root@XT047-hs.(none)>
2017-02-23 18:16:59 +0800

    2017-02-23

---
+++ ch-03/test_3.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int max_int = 999999999999;
+	double min_float = 0.1;
+	float max_float = 0.00000000000000000000001;
+	printf("max_int is %d\n",max_int);
+	printf("min_float is %f\n",min_float);
+	printf("max_int is %f\n",max_float);
+	return 0;
+}
+	 
